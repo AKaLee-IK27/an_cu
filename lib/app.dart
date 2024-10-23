3d06f500411a42041e56fa2c14a913ae9054beec
Anh Khoi Le <khoile0908540@gmail.com>
2024-10-23 19:57:19 +0700

    Add flutter_riverpod and flutter_svg dependencies

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
+        appBar: AppBar(
+            title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                const Icon(Icons.list),
+                const SizedBox(width: 10),
+                SvgPicture.asset('assets/images/logo.svg'),
+              ],
+            ),
+            const Text('Đăng nhập')
+          ],
+        )),
+        body: const Center(
           child: Text('Hello World!'),
         ),
       ),
