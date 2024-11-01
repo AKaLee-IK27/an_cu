56f8f73b4147891f2182e211210b5b2dc3108eb0
Anh Khoi Le <khoile0908540@gmail.com>
2024-11-01 22:20:15 +0700

    Refactor PropertyCard widget: replace Container with InkWell for tap functionality; restructure layout for improved readability and maintainability

--- lib/Views/Home/Widgets/property_card.dart
+++ lib/Views/Home/Widgets/property_card.dart
@@ -9,138 +9,144 @@ class PropertyCard extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 8),
-      constraints: const BoxConstraints(
-        maxHeight: 270,
-      ),
-      padding: const EdgeInsets.symmetric(
-        vertical: 16,
-        horizontal: 20,
-      ),
-      decoration: BoxDecoration(
-        color: AppColors.gray10,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Row(
-        children: [
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                image: const DecorationImage(
-                  image: AssetImage('assets/images/demo_property.jpg'),
-                  fit: BoxFit.cover,
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 8),
+        constraints: const BoxConstraints(
+          maxHeight: 270,
+        ),
+        padding: const EdgeInsets.symmetric(
+          vertical: 16,
+          horizontal: 20,
+        ),
+        decoration: BoxDecoration(
+          color: AppColors.gray10,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  image: const DecorationImage(
+                    image: AssetImage('assets/images/demo_property.jpg'),
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 16,
-                  vertical: 8,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      decoration: const BoxDecoration(
-                        color: AppColors.primary,
-                        shape: BoxShape.circle,
-                      ),
-                      child: IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.favorite_border,
-                          color: AppColors.white,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 8,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        decoration: const BoxDecoration(
+                          color: AppColors.primary,
+                          shape: BoxShape.circle,
+                        ),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.favorite_border,
+                            color: AppColors.white,
+                          ),
                         ),
                       ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                        color: AppColors.secondary,
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: Text(
-                        "Căn hộ",
-                        style:
-                            AppTextStyles.body.copyWith(color: AppColors.white),
-                      ),
-                    )
-                  ],
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 12,
+                          vertical: 8,
+                        ),
+                        decoration: BoxDecoration(
+                          color: AppColors.secondary,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Text(
+                          "Căn hộ",
+                          style: AppTextStyles.body
+                              .copyWith(color: AppColors.white),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          gapW20,
-          Expanded(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Sky Dandelions Apartment",
-                    style: AppTextStyles.title.copyWith(
-                      color: AppColors.secondary,
-                      fontSize: 26,
+            gapW20,
+            Expanded(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Sky Dandelions Apartment",
+                      style: AppTextStyles.title.copyWith(
+                        color: AppColors.secondary,
+                        fontSize: 26,
+                      ),
+                    ),
+                    gapH8,
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.location_on,
+                          color: AppColors.primary,
+                        ),
+                        gapW4,
+                        Text(
+                          "Quận 2, TP.Hồ Chí Minh",
+                          style: AppTextStyles.body,
+                        ),
+                      ],
+                    ),
+                    gapH8,
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.star,
+                          color: AppColors.star,
+                        ),
+                        gapW4,
+                        Text(
+                          "4.5",
+                          style: AppTextStyles.body,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "\$ 290",
+                          style: AppTextStyles.title.copyWith(
+                            color: AppColors.secondary,
+                          ),
+                        ),
+                        TextSpan(
+                          text: "/month",
+                          style: AppTextStyles.body
+                              .copyWith(color: AppColors.secondary),
+                        ),
+                      ],
                     ),
                   ),
-                  gapH8,
-                  Row(
-                    children: [
-                      const Icon(
-                        Icons.location_on,
-                        color: AppColors.primary,
-                      ),
-                      gapW4,
-                      Text(
-                        "Quận 2, TP.Hồ Chí Minh",
-                        style: AppTextStyles.body,
-                      ),
-                    ],
-                  ),
-                  gapH8,
-                  Row(
-                    children: [
-                      const Icon(
-                        Icons.star,
-                        color: AppColors.star,
-                      ),
-                      gapW4,
-                      Text(
-                        "4.5",
-                        style: AppTextStyles.body,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8),
-                child: Text.rich(
-                  TextSpan(
-                    children: [
-                      TextSpan(
-                        text: "\$ 290",
-                        style: AppTextStyles.title.copyWith(
-                          color: AppColors.secondary,
-                        ),
-                      ),
-                      TextSpan(
-                        text: "/month",
-                        style: AppTextStyles.body
-                            .copyWith(color: AppColors.secondary),
-                      ),
-                    ],
-                  ),
                 ),
-              ),
-            ],
-          )),
-        ],
+              ],
+            )),
+          ],
+        ),
       ),
     );
   }
